07432aa2bc08658bc6c05e6254a0a9e91f7389cb
nateL15 <nathanlass@gmail.com>
2017-11-03 10:59:47 -0400

    Add alternating turns for 'X' and 'O'

--- assets/scripts/example.js
+++ assets/scripts/example.js
@@ -1,6 +1,17 @@
-const element = document.createElement('div')
-element.id = 'myDiv'
-element.innerHTML = 'Hello World!'
-document.body.appendChild(element)
+const gameBoard = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-const
+const userClick = ['', '', '', '', '', '', '', '', '']
+
+const playerMove = function () {
+  for (let i = 0; i < 9; i++) {
+    $('#b' + i).on('click', function () {
+      $('#b' + i).html('O')
+    })
+  }
+}
+
+export {
+  gameBoard,
+  userClick,
+  playerMove
+}
--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -7,6 +7,34 @@ $(() => {
   setAPIOrigin(location, config)
 })
 
+const gameBoard = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+const userClick = ['', '', '', '', '', '', '', '', '']
+
+let xMove = true
+
+const playTurn = function () {
+  for (let i = 1; i < 10; i++) {
+    $('#b' + i).on('click', function () {
+      if (xMove === true) {
+        $('#b' + i).text('X')
+        xMove = false
+      } else if (xMove === false) {
+        $('#b' + i).text('O')
+        xMove = true
+      }
+    })
+  }
+}
+
+playTurn()
+
+export {
+  gameBoard,
+  userClick,
+  playTurn
+}
+
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
 
